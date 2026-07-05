cde17dc07950a999532dd09979df814042b909a3
smamerol38730-bit <smamerol38730@liceo.edu.ph>
2026-07-05 20:47:46 +0800

    Add API call using requests

--- api_demo.py
+++ api_demo.py
@@ -1,4 +1,13 @@
 # Sittie Melhayah Amerol
 # MSIT 132 - API Demo
 
-print("This is my API demo file.")
+import requests
+
+url = "https://jsonplaceholder.typicode.com/posts/1"
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    print("Post Title:", data["title"])
+else:
+    print("Error:", response.status_code)
